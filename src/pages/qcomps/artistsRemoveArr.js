a935103994ddefb7f363a029697e326a8847c77f
Yuanyuan Yang <1005842457@qq.com>
2024-02-25 20:34:01 -0500

    Update artistsRemoveArr.js

--- src/pages/qcomps/artistsRemoveArr.js
+++ src/pages/qcomps/artistsRemoveArr.js
@@ -2,14 +2,19 @@ import { useState } from 'react';
 
 let initialArtists = [
   { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+  { id: 1, name: 'Lamidi Olonade Fakeye' },
+  { id: 2, name: 'Louise Nevelson' },
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+  const [artists, setArtists] = useState(initialArtists);
+
+  const handleDelete = (id) => {
+    // Create a new array without the artist to delete
+    const updatedArtists = artists.filter(artist => artist.id !== id);
+    // Set the new array as the new state
+    setArtists(updatedArtists);
+  };
 
   return (
     <>
@@ -18,9 +23,7 @@ export default function List() {
         {artists.map(artist => (
           <li key={artist.id}>
             {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
+            <button onClick={() => handleDelete(artist.id)}>
               Delete
             </button>
           </li>
